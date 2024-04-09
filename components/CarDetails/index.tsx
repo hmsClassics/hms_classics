1bfd948e0671e6e3deb727d6666521f3de834d02
Steve Meisner <steve.meisner@hey.com>
2024-04-08 22:04:36 -0400

    car specs

--- components/CarDetails/index.tsx
+++ components/CarDetails/index.tsx
@@ -1,22 +1,29 @@
+import cx from 'classnames'
 import { ComponentUtilityLabelValue, Maybe } from '@/strapi_api/types'
+import localFont from 'next/font/local'
 import styles from './carDetails.module.scss'
 
 type Props = {
   specs: Maybe<ComponentUtilityLabelValue>[]
 }
 
+const heveticaNueuCondensedBold = localFont({
+  src: '../../fonts/Helvetica-Neue-Condensed-Bold.woff2',
+  variable: '--font-helvetica-neue-condensed-bold',
+})
+
 export default function CarDetails({ specs }: Props) {
   return (
-    <div className={styles.column_one}>
+    <div className={styles.carDetails}>
       {specs?.map((spec) => {
         if (!spec) return null
 
         return (
-          <div className="spec" key={spec.id}>
-            <p>
-              <strong>{spec.label}</strong>
-            </p>
-            <p>{spec.value}</p>
+          <div
+            className={cx(heveticaNueuCondensedBold.className, styles.spec)}
+            key={spec.id}>
+            <p className={styles['spec__title']}>{spec.label}</p>
+            <p className={styles['spec__value']}>{spec.value}</p>
           </div>
         )
       })}
